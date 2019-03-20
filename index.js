dd5d0e8e26896b60ade298884c88172db0a9ffc7
Mads Moi-Aune <madsaune@gmail.com>
2019-03-20 22:34:59 +0100

    Adds authentication with mongodb

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const config = require('./config');
 
 const app = express();
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
+let userSchema = new mongoose.Schema({
+    username: String,
+    name: String,
+    email: String,
+    password: String,
+    permissions: Object,
+    active: Boolean,
+    tokenLastIssued: Date,
+    createdOn: Date,
+    updateOn: Date 
+});
+
+userSchema.methods.verify = function(password) {
+    return this.password === password;
+};
+
+let User = mongoose.model('User', userSchema);
 
 function verifyToken(req, res, next) {
     const token = req.headers['x-access-token'];
@@ -18,34 +38,39 @@ function verifyToken(req, res, next) {
     });
 }
 
-app.get('/api/auth/verify', verifyToken, (req, res) => res.send(200));
+app.get('/api/auth/verify', verifyToken, (req, res) => res.sendStatus(200));
 
 app.post('/api/auth/login', (req, res) => {
     
     const username = req.body.username;
     const password = req.body.password;
 
-    console.log("Username: " + username);
-    console.log("Password: " + password);
+    User.findOne({ username: username }, (err, user) => {
+        if (err) return res.sendStatus(500);
+        if (user === null) return res.status(404).json({ "auth": false, "message": "Incorrect username or password." });
 
-    let user = {
-        "name": "Username",
-        "username": username
-    };
-
-    const token = jwt.sign(user, config.jwtsecret, {
-        "expiresIn": 86400 // 24 hours
-    });
+        if (user.verify(password)) {
+            const payload = {
+                "username": user.username,
+                "name": user.name,
+                "email": user.email
+            };
     
-    res.json({
-        "error": false,
-        "token": token,
-        "user": user
+            const token = jwt.sign(payload, config.jwtsecret, {
+                "expiresIn": 86400 // 24 hours
+            });
+
+            res.status(200).json({
+                "auth": true,
+                "token": token,
+                "user": user
+            });
+        }
     });
 });
 
 app.post('/api/protected', verifyToken, (req, res) => {
-    res.json({
+    res.status(200).json({
         "error": false,
         "decoded": req.user
     });
