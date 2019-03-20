4c1f191362e9e43fdc0435797312b42e2b5dcbde
Mads Moi-Aune <madsaune@gmail.com>
2019-03-20 21:19:18 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    "jwtsecret": "jhfds87fsdfgdsfghdfiungdufngdh9f8g7d98fnrifuoaunfhsdacsc9s8ca09s8d9084324uh23u4ih"
+}
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const config = require('./config');
+
+const app = express();
+app.use(bodyParser.json());
+
+function verifyToken(req, res, next) {
+    const token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ "auth": false, "message": "No token provided." });
+
+    jwt.verify(token, config.jwtsecret, (err, decoded) => {
+        if (err) return res.status(401).send({ "auth": false, "message": "Token is not valid." });
+
+        next();
+    });
+}
+
+app.get('/api/auth/verify', verifyToken, (req, res) => res.send(200));
+
+app.post('/api/auth/login', (req, res) => {
+    
+    const username = req.body.username;
+    const password = req.body.password;
+
+    console.log("Username: " + username);
+    console.log("Password: " + password);
+
+    let user = {
+        "name": "Username",
+        "username": username
+    }
+
+    const token = jwt.sign(user, config.jwtsecret, {
+        "expiresIn": 86400 // 24 hours
+    });
+    
+    res.json({
+        "error": false,
+        "token": token,
+        "user": user
+    });
+});
+
+app.post('/api/send/sms', verifyToken, (req, res) => {
+    res.json({
+        "error": false
+    })
+});
+
+app.listen(8080, () => console.log("Server listening on port 8080..."));
